62dd37908254380dfc734e6f46eaf637fbb4dbb2
InspectorGadget <igadget28@gmail.com>
2017-02-11 16:26:33 +0800

    Final Commit

--- src/RTG/JDT/Loader.php
+++ src/RTG/JDT/Loader.php
@@ -42,12 +42,24 @@ class Loader extends PluginBase implements Listener {
                         
                         case "help":
                             
-                            $sender->sendMessage(" -- JD Transfer 1.0 Beta -- ");
+                            $sender->sendMessage(" -- JD Transfer 1.1 -- ");
                             $sender->sendMessage(" ");
                             $sender->sendMessage(TF::RED . "[Transfer] /transfer [ServerName]");
-                            $sender->sendMessage(TF::RESET . " - Available Servers - ");
+                            $sender->sendMessage(" ");
+                            $sender->sendMessage(TF::YELLOW . " - Available Servers - ");
                             $sender->sendMessage(" ");
                             $sender->sendMessage(TF::RESET . " - JDC");
+                            $sender->sendMessage(TF::RESET . " - BC");
+                            $sender->sendMessage(TF::RESET . " - DC");
+                            $sender->sendMessage(TF::RESET . " - IB");
+                            $sender->sendMessage(TF::RESET . " - JDB");
+                            $sender->sendMessage(TF::RESET . " - JDE");
+                            $sender->sendMessage(TF::RESET . " - JDG");
+                            $sender->sendMessage(TF::RESET . " - JDMulti");
+                            $sender->sendMessage(TF::RESET . " - JDU");
+                            $sender->sendMessage(TF::RESET . " - JDK");
+                            $sender->sendMessage(" ");
+                            $sender->sendMessage(TF::RED . "Please note that most of the servers are 'whitelisted' or some may not be online!. Please don't spam this feature!");
                             
                             return true;
                         break;
@@ -56,6 +68,87 @@ class Loader extends PluginBase implements Listener {
                             
                             $ip = "jdcraft.net";
                             $port = 19132;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "BC":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 20000;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "DC":
+                            
+                            $ip = "jdcaft.net";
+                            $port = 41414;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "IB":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 25252;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                        
+                        case "JDB":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 33333;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                        
+                        case "JDE":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 19133;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "JDG":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 55555;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "JDMulti":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 17171;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "JDU":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 12321;
+                            $this->onTransfer($sender, $ip, $port);
+                            
+                            return true;
+                        break;
+                    
+                        case "JDK":
+                            
+                            $ip = "jdcraft.net";
+                            $port = 11111;
                             $this->onTransfer($sender, $ip, $port);
                             
                             return true;

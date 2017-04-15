9e1100922d470b061481216136bc270c92c8940a
RTG <RTGDaCoder@users.noreply.github.com>
2017-04-15 21:34:41 +0800

    Create Travis.php

---
+++ Travis.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * MyMoney is licensed under the BoxOfDevs license 1.1.2
+ *
+ * By using MyMoney you automatically agree to the license.
+ */
+$server = proc_open(PHP_BINARY.' src/pocketmine/PocketMine.php --no-wizard --disable-readline', [
+    0 => ['pipe', 'r'],
+    1 => ['pipe', 'w'],
+    2 => ['pipe', 'w'],
+], $pipes);
+fwrite($pipes[0], "version\nmakeplugin JDTransfer\nstop\n\n");
+while (!feof($pipes[1])) {
+    echo fgets($pipes[1]);
+}
+fclose($pipes[0]);
+fclose($pipes[1]);
+fclose($pipes[2]);
+echo "\n\nReturn value: ".proc_close($server)."\n";
+if (count(glob('plugins/DevTools/JDTransfer*.phar')) === 0) {
+    echo "Failed to create JDTransfer phar!\n";
+    exit(1);
+} else {
+    echo "MyMoney phar created!\n";
+    exit(0);
+}

42591bd67c8b49285ec0f55a2361de306438f37b
RTG <igadget28@gmail.com>
2017-05-05 14:46:08 +0800

    Major Bump

--- src/RTG/JDT/Loader.php
+++ src/RTG/JDT/Loader.php
@@ -1,174 +1,87 @@
 <?php
 
+// some from AddWindow plugin!
+
 namespace RTG\JDT;
 
-/* Essentials */
-use pocketmine\Server;
-use pocketmine\plugin\PluginBase;
-use pocketmine\Player;
-use pocketmine\network\protocol\TransferPacket;
-use pocketmine\command\CommandExecutor;
-use pocketmine\event\Listener;
+use pocketmine\block\Block;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\level\Position;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\math\Vector3;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\nbt\tags\IntArrayTag;
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\Chest;
+use pocketmine\tile\Tile;
+use pocketmine\event\Cancellable;
+use pocketmine\inventory\Inventory;
+use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\entity\Entity;
+use pocketmine\network\protocol\TransferPacket;
+use pocketmine\event\block\BlockPlaceEvent;
 
-class Loader extends PluginBase implements Listener {
-    
-    public function onEnable() {
+class Loader extends PluginBase implements Listener{
+
+    public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
     
-    public function onTransfer(Player $p, $ip, $port) {
-        
+    public function sendChestInventory(Player $player){
+        $nbt = new CompoundTag('', [
+                new StringTag('id', Tile::CHEST),
+                new IntTag('Server Menu', 1),
+                new IntTag('x', floor($player->x)),
+                new IntTag('y', floor($player->y) - 4),
+                new IntTag('z', floor($player->z))
+        ]);
+		
+        $tile = Tile::createTile('Chest', $player->getLevel(), $nbt);
+        $block = Block::get(Block::CHEST);
+        $block->x = floor($tile->x);
+        $block->y = floor($tile->y);
+        $block->z = floor($tile->z);
+        $block->level = $tile->getLevel();
+        $block->level->sendBlocks([$player], [$block]);
+        $test = Item::get(279, 0, 1);
+        $test->setCustomName("JDCraft");
+        $tile->getInventory()->setItem(0, $test);
+        $player->addWindow($tile->getInventory());
+    }
+
+    public function onTransfer(Player $p, $ip, $port) {       
         $pk = new \pocketmine\network\protocol\TransferPacket();
         $pk->address = $ip;
         $pk->port = (int) $port;
         $p->dataPacket($pk);
-        $p->sendMessage("[Transfer] Executing...");
-        $name = $p->getName();
-        $this->getServer()->broadcastMessage(TF::GREEN . "[Server] Transfering " . TF::RED . "$name " . TF::GREEN . "to the server the player requested using JDTransfer!");
-        
     }
-    
-    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-        
-        switch(strtolower($command->getName())) {
-            
-            case "transfer":
-                
-                if(isset($args[0])) {
-                    switch($args[0]) {
-                        
-                        case "help":
-                            
-                            $sender->sendMessage(" -- JD Transfer 1.1 -- ");
-                            $sender->sendMessage(" ");
-                            $sender->sendMessage(TF::RED . "[Transfer] /transfer [ServerName]");
-                            $sender->sendMessage(" ");
-                            $sender->sendMessage(TF::YELLOW . " - Available Servers - ");
-                            $sender->sendMessage(" ");
-                            $sender->sendMessage(TF::RESET . " - JDC");
-                            $sender->sendMessage(TF::RESET . " - BC");
-                            $sender->sendMessage(TF::RESET . " - DC");
-                            $sender->sendMessage(TF::RESET . " - IB");
-                            $sender->sendMessage(TF::RESET . " - JDB");
-                            $sender->sendMessage(TF::RESET . " - JDE");
-                            $sender->sendMessage(TF::RESET . " - JDG");
-                            $sender->sendMessage(TF::RESET . " - JDMulti");
-                            $sender->sendMessage(TF::RESET . " - JDU");
-                            $sender->sendMessage(TF::RESET . " - JDK");
-                            $sender->sendMessage(" ");
-                            $sender->sendMessage(TF::RED . "Please note that most of the servers are 'whitelisted' or some may not be online!. Please don't spam this feature!");
-                            
-                            return true;
-                        break;
-                        
-                        case "JDC":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 19132;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "BC":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 20000;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "DC":
-                            
-                            $ip = "jdcaft.net";
-                            $port = 41414;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "IB":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 25252;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                        
-                        case "JDB":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 33333;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                        
-                        case "JDE":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 19133;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "JDG":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 55555;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "JDMulti":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 17171;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "JDU":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 12321;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                    
-                        case "JDK":
-                            
-                            $ip = "jdcraft.net";
-                            $port = 11111;
-                            $this->onTransfer($sender, $ip, $port);
-                            
-                            return true;
-                        break;
-                         
-                    }
-                       
-                }
-                else {
-                    $sender->sendMessage("Usage: /transfer help");
-                }
-                
-                return true;
-            break;
-   
-        }
-          
+
+    public function onCheck(EntityInventoryChangeEvent $event){ // Item Use Checker!
+        $player = $event->getEntity();
+           $newItem = $event->getNewItem();
+           if($newItem->getName() === "JDCraft"){
+                $event->setCancelled();
+                $this->onTransfer($player, 'jdcraft.net', 19132);
+                  return;
+          }
     }
-    
-    public function onDisable() {
-        $this->getLogger()->warning("Turning off JDTransfer!");
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+            if($sender instanceof Player){
+              switch(strtolower($cmd->getName())){
+                case "jdt":
+                      $sender->sendMessage("Added chest window!");
+                      $this->sendChestInventory($sender);
+                break;
+              }
+            }
     }
 }

f2610bc0c2be502ef9e39e7b46b774eedc69f154
RTG <igadget28@gmail.com>
2017-05-05 22:42:39 +0800

    Added Player Count support

--- src/RTG/JDT/Loader.php
+++ src/RTG/JDT/Loader.php
@@ -28,11 +28,16 @@ use pocketmine\event\entity\EntityInventoryChangeEvent;
 use pocketmine\entity\Entity;
 use pocketmine\network\protocol\TransferPacket;
 use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\utils\TextFormat as TF;
 
 class Loader extends PluginBase implements Listener{
+    
+    private $fetchedData;
+    private $server;
 
     public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);        
+        
     }
     
     public function sendChestInventory(Player $player){
@@ -53,11 +58,33 @@ class Loader extends PluginBase implements Listener{
         $block->level->sendBlocks([$player], [$block]);
         $JDC = Item::get(279, 0, 1);
         $JDE = Item::get(279, 0, 1);
-        $JDC->setCustomName("JDCraft : 19132");
-        $JDE->setCustomName("JDEnterprise : 19133");
+        $BC = Item::get(279, 0, 1);
+        $JDC->setCustomName(TF::YELLOW . "JDCraft : 19132\n " . TF::RED . $this->getPl('jdcraft.net', 19132) . " players online!");
+        $JDE->setCustomName(TF::GREEN . "JDEnterprise : 19133\n " . TF::RED . $this->getPl('jdcraft.net', 19133) . " players online!");
+        $BC->setCustomName(TF::RED . "BlazeCraft : 20000\n " . TF::RED . $this->getPl('jdcraft.net', 20000) . " players online!");
         $tile->getInventory()->setItem(0, $JDC);
         $tile->getInventory()->setItem(2, $JDE);
+        $tile->getInventory()->setItem(4, $BC);
         $player->addWindow($tile->getInventory());
+    }
+    
+    public function query($host, $port) {
+        
+        $this->server = $this->UT3Query($host, $port);
+        
+        if ($this->server === null) {
+            return true;
+        }
+        
+        $this->fetchedData = [
+            'server_on' => $this->server[15]
+        ];
+            
+    }
+        
+    public function getPl($host, $port) {
+        $this->query($host, $port);
+        return $this->fetchedData['server_on'];
     }
 
     public function onTransfer(Player $p, $ip, $port) {       
@@ -70,17 +97,47 @@ class Loader extends PluginBase implements Listener{
     public function onCheck(EntityInventoryChangeEvent $event){ //
         $player = $event->getEntity();
            $newItem = $event->getNewItem();
-           if($newItem->getName() === "JDCraft : 19132"){
+           if($newItem->getName() === TF::YELLOW . "JDCraft : 19132"){
                $event->setCancelled();
                $this->onTransfer($player, 'jdcraft.net', 19132);
                   return;
-          } elseif ($newItem->getName() === "JDEnterprise : 19133") {
+          } elseif ($newItem->getName() === TF::GREEN . "JDEnterprise : 19133") {
               $event->setCancelled();
               $this->onTransfer($player, 'jdcraft.net', 19133);
+          } elseif ($newItem->getName() === TF::RED . "BlazeCraft : 20000") {
+              $event->setCancelled();
+              $this->onTransfer($player, 'jdcraft.net', 20000);
           }
+          
     }
-
-
+    
+    private function UT3Query($host, $port) {
+        
+        $socket = @fsockopen("udp://" . $host, $port);
+        if (!$socket)
+            return null;
+        $online = @fwrite($socket, "\xFE\xFD\x09\x10\x20\x30\x40\xFF\xFF\xFF\x01");
+        if (!$online)
+            return null;
+        $challenge = @fread($socket, 1400);
+        if (!$challenge)
+            return null;
+        $challenge = substr(preg_replace("/[^0-9-]/si", "", $challenge), 1);
+        $query = sprintf("\xFE\xFD\x00\x10\x20\x30\x40%c%c%c%c\xFF\xFF\xFF\x01",
+            $challenge >> 24, $challenge >> 16, $challenge >> 8, $challenge >> 0);
+        if (!@fwrite($socket, $query))
+            return null;
+        $response = array();
+        $response[] = @fread($socket, 2048);
+        $response = implode($response);
+        $response = substr($response, 16);
+        $response = explode("\0", $response);
+        array_pop($response);
+        array_pop($response);
+        return $response;
+        
+    }
+    
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
             if($sender instanceof Player){
               switch(strtolower($cmd->getName())){
